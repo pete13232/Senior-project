c0cc4887c760b7e029d453785c7907c37adcd1c4
Pete <peetawit.vong@mail.kmutt.ac.th>
2023-03-09 23:22:02 +0700

    CREATE | auto create validateLog

--- Back_End/routes/animations.js
+++ Back_End/routes/animations.js
@@ -2,46 +2,79 @@ const express = require('express')
 const router = express.Router()
 
 const Animation = require('../models/Animation')
+const ValidateLog = require("../models/ValidateLog")
+const User = require('../models/User')
 
 router.route("/")
     .get((req, res) => {
         Animation.find({}).populate({
             path: "validateLog",
-            select:{animation: 0},
+            select: { animation: 0, _id: 0 },
             populate: {
                 path: "user",
                 select: {
+                    _id: 0,
                     username: 1
                 }
             }
-        }).then((success, err) => {
-            if (success) {
-                console.log(success[0])
-                res.send(success)
-            }
-            else res.send(err)
+        }).then(doc => {
+            // console.log(doc[0])
+            res.send(doc)
+        }).catch(err => {
+            res.send(err)
         })
     })
 
 // Create New Animation
 router.route("/add")
     .post((req, res) => {
+        const word = req.body.word
+        const file = req.body.file
         const newAnimation = Animation({
-            word: req.body.word,
-            file: req.body.file,
+            word: word,
+            file: file,
             validateLog: null
         })
-        newAnimation.save().then((success, err) => {
-            if (success) res.send(success)
-            else res.send(err)
-        })
+        newAnimation.save()
+            .then(doc => res.send(doc))
+            .catch(err => res.send(err))
     })
 
 // Add Validate log to Selected Animation
 router.route("/validate/:animationID")
     .put((req, res) => {
-        Animation.findByIdAndUpdate({ _id: req.params.animationID }, { $set: { validateLog: req.body.validateID } }, { new: true }).then((doc, err) => {
-            if (doc) res.send(doc)
+        const animationID = req.params.animationID
+        const validateLog = req.body.validateID
+        Animation.findByIdAndUpdate({ _id: animationID }, { $set: { validateLog: validateLog } }, { new: true })
+            .then((doc) => {
+                res.send(doc)
+            }).catch(err => {
+                res.send(err)
+            })
+    })
+    .post((req, res) => {
+        const animationID = req.params.animationID
+        const userID = req.body.userID
+        const validateStat = req.body.validateStat
+        const newValidateLog = ValidateLog({
+            animation: animationID,
+            user: userID,
+            validateStat: validateStat
+        })
+        newValidateLog.save().then((data, err) => {
+            if (data) {
+                User.findByIdAndUpdate({ _id: userID }, { $push: { validateLog: data._id } })
+                    .then(() => {
+                        Animation.findByIdAndUpdate({ _id: animationID }, { $set: { validateLog: data._id } })
+                            .then(() => {
+                                res.send(data)
+                            }).catch(err => {
+                                res.send(err)
+                            })
+                    }).catch(err => {
+                        res.send(err)
+                    })
+            }
             else res.send(err)
         })
     })
@@ -49,7 +82,8 @@ router.route("/validate/:animationID")
 // Delete Selected Animation
 router.route("/delete/:animationID")
     .delete((req, res) => {
-        Animation.deleteOne({ _id: req.params.animationID }, (err, result) => {
+        const animationID = req.params.animationID
+        Animation.deleteOne({ _id: animationID }, (err, result) => {
             if (!err && result.deletedCount != 0) {
                 res.send("Animation deleted success !")
             } else if (result.deletedCount == 0) {
