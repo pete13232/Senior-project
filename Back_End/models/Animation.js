46df5403d75af276bcb77a9829f75a64fb54796a
Pete <peetawit.vong@mail.kmutt.ac.th>
2023-03-11 15:53:37 +0700

    UPDATE | add require condition to ani schema

--- Back_End/models/Animation.js
+++ Back_End/models/Animation.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose')
 
 // Create Model (Compiled from Schema, instance of model is "Document")
 const AnimationSchema = new mongoose.Schema({
-    word: { type: String },
-    file: { type: mongoose.Schema.Types.Mixed },
+    word: { type: String, require: true },
+    file: { type: mongoose.Schema.Types.Mixed, require: true },
     validateLog: {
         type: mongoose.Schema.Types.ObjectId,
         ref: "ValidateLog",
