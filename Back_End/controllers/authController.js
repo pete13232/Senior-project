1495fc3b1442da96d5eda58ac6b3f75685c9a6d0
Pete <peetawit.vong@mail.kmutt.ac.th>
2023-05-02 16:10:43 +0700

    UPDATE | change cookie to local & signup for admin

--- Back_End/controllers/authController.js
+++ Back_End/controllers/authController.js
@@ -52,8 +52,12 @@ const signup_post = async (req, res) => {
       role,
     });
     const token = createToken(newUser._id);
-    res.cookie("jwt", token, { httpOnly: true, maxAge: maxAge * 1000 });
-    res.status(201).json({ newUser: newUser });
+    // res.cookie("jwt", token, { httpOnly: true, maxAge: maxAge * 1000 });
+    // res.status(201).json({ newUser: newUser });
+    res.status(200).json({
+      token: token,
+      newUser: newUser
+    })
   } catch (err) {
     const errors = handleErrors(err);
     res.status(400).json({ errors });
@@ -63,31 +67,54 @@ const signup_post = async (req, res) => {
 const login_post = async (req, res) => {
   const { username, password } = req.body;
 
-    try {
-        const newUser = await User.login(username, password)
-        const token = createToken(newUser._id)
+  try {
+    const newUser = await User.login(username, password)
+    const token = createToken(newUser._id)
 
-        // for front-end domain localhost:3000 
-        //  // for back-end domain localhost:3333 
-        // res.cookie('jwt', token, {
-        //     httpOnly: true,
-        //     maxAge: maxAge * 1000,
-        // })
-        res.status(200).json({
-            token: token,
-            newUser: {
-                _id: newUser._id,
-                username: newUser.username,
-                role: newUser.role,
-                firstName: newUser.firstName,
-                lastName: newUser.lastName
-            }
-        })
-    } catch (err) {
-        const errors = handleErrors(err)
-        res.status(400).json({ errors })
-    }
+    // for front-end domain localhost:3000 
+    //  // for back-end domain localhost:3333 
+    // res.cookie('jwt', token, {
+    //     httpOnly: true,
+    //     maxAge: maxAge * 1000,
+    // })
+    res.status(200).json({
+      token: token,
+      newUser: {
+        _id: newUser._id,
+        username: newUser.username,
+        role: newUser.role,
+        firstName: newUser.firstName,
+        lastName: newUser.lastName
+      }
+    })
+  } catch (err) {
+    const errors = handleErrors(err)
+    res.status(400).json({ errors })
+  }
 }
+
+const signupAdmin_post = async (req, res) =>{
+  const { username, password, firstName, lastName, role } = req.body;
+
+  try {
+    const newUser = await User.create({
+      username,
+      password,
+      firstName,
+      lastName,
+      role,
+    });
+    const token = createToken(newUser._id);
+    res.status(200).json({
+      token: token,
+      newUser: newUser
+    })
+  } catch (err) {
+    const errors = handleErrors(err);
+    res.status(400).json({ errors });
+  }
+}
+
 
 const signup_get = (req, res) => {
   res.render("signup");
@@ -98,8 +125,8 @@ const login_get = (req, res) => {
 };
 
 const logout_get = (req, res) => {
-    res.cookie('jwt', '', { maxAge: 1 })
-    res.redirect('/')
+  res.cookie('jwt', '', { maxAge: 1 })
+  res.redirect('/')
 }
 
-module.exports = { signup_post, login_post, signup_get, login_get, logout_get };
+module.exports = { signup_post, login_post, signup_get, login_get, logout_get, signupAdmin_post };
